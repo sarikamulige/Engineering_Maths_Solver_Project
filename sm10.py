a3a56ce97c4e0675c6631c53a3ea73771dccf15f
sarikamulige <sarikamulige@gmail.com>
2026-03-10 19:44:27 +0530

    Update sm10.py

--- sm10.py
+++ sm10.py
@@ -713,4 +713,6 @@ P(x) = yn + pΔyn + p(p+1)/2! Δ²yn + ... + p(p+1)...(p+n-1)/n! Δⁿyn
 """
     return render_template_string(html)
 if __name__ == "__main__":
-    app.run(debug=True)
+    port=int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0",port=port)
+   
